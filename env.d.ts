629384fc03a959dfb43067859f5f3763f4495602
tomoyasakamoto32 <oregairu0302@gmail.com>
2024-02-12 23:12:17 +0900

    ciで環境変数を読むように修正

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly API_KEY: string;
+    readonly MICROCMS_SERVICE_DOMAIN: string;
   }
 }
--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -1,8 +1,6 @@
 import { createClient } from 'microcms-js-sdk';
 
-const MICROCMS_SERVICE_DOMAIN = 'aq9lf0scld';
-
 export const client = createClient({
-  serviceDomain: MICROCMS_SERVICE_DOMAIN,
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: process.env.API_KEY,
 });
