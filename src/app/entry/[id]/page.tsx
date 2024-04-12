d0cbd34d324b1f0bdf23d46096015813da2e8907
坂本 友哉sakamoto tomoya <oregairu0302@gmail.com>
2024-04-12 13:52:38 +0900

    暫定

--- src/app/entry/[id]/page.tsx
+++ src/app/entry/[id]/page.tsx
@@ -1,28 +1,28 @@
-import * as cheerio from 'cheerio';
+// import * as cheerio from 'cheerio';
 import hljs from 'highlight.js/lib/core';
 import css from 'highlight.js/lib/languages/css';
 import typescript from 'highlight.js/lib/languages/typescript';
 import { Metadata, ResolvingMetadata } from 'next';
 
-import styles from './page.module.css';
+// import styles from './page.module.css';
 
 import { getBlog } from '@/app/lib/apis/getBlog';
-import { Blog } from '@/app/types/blogs';
-import TableOfContents from '@/app/ui/TableOfContents';
+// import { Blog } from '@/app/types/blogs';
+// import TableOfContents from '@/app/ui/TableOfContents';
 
 import 'highlight.js/styles/hybrid.css';
 
 hljs.registerLanguage('typescript', typescript);
 hljs.registerLanguage('css', css);
 
-type PlainBlogDetailProps = {
-  blog: Blog;
-  headings: {
-    tagName: string;
-    id: string;
-    text: string | undefined;
-  }[];
-};
+// type PlainBlogDetailProps = {
+//   blog: Blog;
+//   headings: {
+//     tagName: string;
+//     id: string;
+//     text: string | undefined;
+//   }[];
+// };
 
 type Props = {
   params: { id: string };
@@ -49,53 +49,58 @@ export async function generateMetadata({ params }: Props, parent: ResolvingMetad
 }
 
 // コンテンツのダウンロードに時間がかかり過ぎているのでchakra-uiを剥がした
-const PlainBlogDetail = ({ blog, headings }: PlainBlogDetailProps) => (
-  <div className={styles['p-content-detail']}>
-    <div className={styles['p-content-detail__article']}>
-      <div className={styles['p-content-detail__article-title-wrapper']}>
-        <h1 className={styles['p-content-detail__article-title']}>{blog.title}</h1>
-        <div className={styles['p-content-detail__article-tag-list']}>
-          {blog.categories?.map((category) => (
-            <span key={category?.id} className={styles['p-content-detail__article-tag']}>
-              {category?.name}
-            </span>
-          ))}
-        </div>
-        <div className={styles['p-content-detail__article-date-wrapper']}>
-          <p className={styles['p-content-detail__article-date']}>{blog.formattedPublishedAt} 公開</p>
-          <p className={styles['p-content-detail__article-date']}>{blog.formattedUpdatedAt} 更新</p>
-        </div>
-      </div>
-      <div className={styles['p-content-detail__toc-mobile']}>
-        <TableOfContents headings={headings} isMobile />
-      </div>
-      {/* eslint-disable-next-line react/no-danger */}
-      <div dangerouslySetInnerHTML={{ __html: blog.content ?? '' }} className={styles.content} />
-    </div>
-    <div className={styles['p-content-detail__toc']}>
-      <TableOfContents headings={headings} />
-    </div>
-  </div>
-);
+// const PlainBlogDetail = ({ blog, headings }: PlainBlogDetailProps) => (
+//   <div className={styles['p-content-detail']}>
+//     <div className={styles['p-content-detail__article']}>
+//       <div className={styles['p-content-detail__article-title-wrapper']}>
+//         <h1 className={styles['p-content-detail__article-title']}>{blog.title}</h1>
+//         <div className={styles['p-content-detail__article-tag-list']}>
+//           {blog.categories?.map((category) => (
+//             <span key={category?.id} className={styles['p-content-detail__article-tag']}>
+//               {category?.name}
+//             </span>
+//           ))}
+//         </div>
+//         <div className={styles['p-content-detail__article-date-wrapper']}>
+//           <p className={styles['p-content-detail__article-date']}>{blog.formattedPublishedAt} 公開</p>
+//           <p className={styles['p-content-detail__article-date']}>{blog.formattedUpdatedAt} 更新</p>
+//         </div>
+//       </div>
+//       <div className={styles['p-content-detail__toc-mobile']}>
+//         <TableOfContents headings={headings} isMobile />
+//       </div>
+//       {/* eslint-disable-next-line react/no-danger */}
+//       <div dangerouslySetInnerHTML={{ __html: blog.content ?? '' }} className={styles.content} />
+//     </div>
+//     <div className={styles['p-content-detail__toc']}>
+//       <TableOfContents headings={headings} />
+//     </div>
+//   </div>
+// );
 
-const BlogDetail = async ({ params }: { params: { id: string } }) => {
-  const blog = await getBlog(params.id);
-  const $ = cheerio.load(blog.content ?? '');
-  const headings = $('h2, h3').toArray();
-  const toc = headings.map((data) => ({
-    text: $(data.children[0]).text(),
-    id: data.attribs.id,
-    tagName: data.name,
-  }));
+// const BlogDetail = async ({ params }: { params: { id: string } }) => {
+//   const blog = await getBlog(params.id);
+//   const $ = cheerio.load(blog.content ?? '');
+//   const headings = $('h2, h3').toArray();
+//   const toc = headings.map((data) => ({
+//     text: $(data.children[0]).text(),
+//     id: data.attribs.id,
+//     tagName: data.name,
+//   }));
 
-  $('pre code').each((_, elm) => {
-    // const lang = $(elm).attr('class')?.split('-')[1];
-    const result = hljs.highlight($(elm).text(), { language: 'typescript' });
-    $(elm).html(result.value);
-    $(elm).addClass('hljs');
-  });
+//   $('pre code').each((_, elm) => {
+//     // const lang = $(elm).attr('class')?.split('-')[1];
+//     const result = hljs.highlight($(elm).text(), { language: 'typescript' });
+//     $(elm).html(result.value);
+//     $(elm).addClass('hljs');
+//   });
 
-  return <PlainBlogDetail blog={{ ...blog, content: $.html() }} headings={toc} />;
+//   return <PlainBlogDetail blog={{ ...blog, content: $.html() }} headings={toc} />;
+// };
+
+const BlogDetail = () => {
+  console.log('aaaa');
+  return <div>aaaaa</div>;
 };
 
 export default BlogDetail;
